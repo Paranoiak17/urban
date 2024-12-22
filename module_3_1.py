93037a7a9125412f0575c542baed16e058da66a3
Paranoiak17 <54306168+Paranoiak17@users.noreply.github.com>
2024-12-22 11:43:38 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,27 @@
+# Задача "Счётчик вызовов"
+
+calls = 0
+
+def count_calls():
+    global calls
+    calls = calls + 1
+
+def string_info(string):
+    count_calls()
+    tuple1 = (len(string), string.upper(), string.lower())
+    return tuple1
+
+def is_contains(string, list_to_search):
+    count_calls()
+    result = False
+    string = string.lower()
+    for element in list_to_search:
+        if string == element.lower():
+            result = True
+    return result
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
