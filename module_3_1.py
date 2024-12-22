59b3db14b0a62e762288f6962190699119532d57
Paranoiak17 <54306168+Paranoiak17@users.noreply.github.com>
2024-12-22 11:50:03 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -4,19 +4,17 @@ calls = 0
 
 def count_calls():
     global calls
-    calls = calls + 1
+    calls += 1
 
 def string_info(string):
     count_calls()
-    tuple1 = (len(string), string.upper(), string.lower())
-    return tuple1
+    return (len(string), string.upper(), string.lower())
 
 def is_contains(string, list_to_search):
     count_calls()
     result = False
-    string = string.lower()
     for element in list_to_search:
-        if string == element.lower():
+        if string.lower() == element.lower():
             result = True
     return result
 
